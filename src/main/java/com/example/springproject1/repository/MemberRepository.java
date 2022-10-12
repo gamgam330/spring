b87cd7885b312a369e65299eefa62e4234c0ad67
gyumin <gamgam0330@naver.com>
2022-10-12 20:01:07 +0900

    Third commit

---
+++ src/main/java/com/example/springproject1/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.springproject1.repository;
+
+import com.example.springproject1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
