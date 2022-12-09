736ffdafa20015eca383b0892c7f9af831e54ffd
90000e <48207536+gamgam330@users.noreply.github.com>
2022-12-09 21:05:44 +0900

    [SPRING]

--- AppConfig.java
+++ AppConfig.java
@@ -1,21 +1,28 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.*;
 
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderSerice orderSerice(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
     //DIP 위반 해결 공연관리자가 배우를 섭외함.
 
+    //AppConfig 리팩터링을 하게되면 AppConfig코드만 봐도 역할이 다 드러남
 }
